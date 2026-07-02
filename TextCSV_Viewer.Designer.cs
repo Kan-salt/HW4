d7ffa7def678bac830919bad03dd97a8983c126f
Kraivid <kaivish0000@gmail.com>
2026-07-02 19:53:05 +0700

    "Commit final form"

---
+++ TextCSV_Viewer.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace FileProcessing
+{
+	partial class frmTextView
+	{
+		/// <summary>sd
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbFileName = new System.Windows.Forms.TextBox();
+            this.btBrowse = new System.Windows.Forms.Button();
+            this.tabcMain = new System.Windows.Forms.TabControl();
+            this.tabpText = new System.Windows.Forms.TabPage();
+            this.rtbShow = new System.Windows.Forms.RichTextBox();
+            this.btRead = new System.Windows.Forms.Button();
+            this.tabpCSV = new System.Windows.Forms.TabPage();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.RegisterDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SHA256_Hash = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MD5_Hash = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SHA1_Hash = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btReadCSV = new System.Windows.Forms.Button();
+            this.tabcMain.SuspendLayout();
+            this.tabpText.SuspendLayout();
+            this.tabpCSV.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File";
+            // 
+            // tbFileName
+            // 
+            this.tbFileName.Location = new System.Drawing.Point(93, 11);
+            this.tbFileName.Name = "tbFileName";
+            this.tbFileName.Size = new System.Drawing.Size(809, 22);
+            this.tbFileName.TabIndex = 1;
+            this.tbFileName.Text = "D:\\ป.ตรี 2\\visual studio workbench path\\HW4\\FileProcessing\\malware_500.csv";
+            this.tbFileName.TextChanged += new System.EventHandler(this.tbFileName_TextChanged);
+            // 
+            // btBrowse
+            // 
+            this.btBrowse.Location = new System.Drawing.Point(936, 6);
+            this.btBrowse.Name = "btBrowse";
+            this.btBrowse.Size = new System.Drawing.Size(100, 27);
+            this.btBrowse.TabIndex = 2;
+            this.btBrowse.Text = "Browse";
+            this.btBrowse.UseVisualStyleBackColor = true;
+            this.btBrowse.Click += new System.EventHandler(this.btBrowse_Click);
+            // 
+            // tabcMain
+            // 
+            this.tabcMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabcMain.Controls.Add(this.tabpText);
+            this.tabcMain.Controls.Add(this.tabpCSV);
+            this.tabcMain.Location = new System.Drawing.Point(11, 38);
+            this.tabcMain.Name = "tabcMain";
+            this.tabcMain.SelectedIndex = 0;
+            this.tabcMain.Size = new System.Drawing.Size(1431, 889);
+            this.tabcMain.TabIndex = 3;
+            // 
+            // tabpText
+            // 
+            this.tabpText.Controls.Add(this.rtbShow);
+            this.tabpText.Controls.Add(this.btRead);
+            this.tabpText.Location = new System.Drawing.Point(4, 25);
+            this.tabpText.Name = "tabpText";
+            this.tabpText.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabpText.Size = new System.Drawing.Size(1423, 860);
+            this.tabpText.TabIndex = 0;
+            this.tabpText.Text = "Text";
+            this.tabpText.UseVisualStyleBackColor = true;
+            // 
+            // rtbShow
+            // 
+            this.rtbShow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbShow.Location = new System.Drawing.Point(12, 50);
+            this.rtbShow.Name = "rtbShow";
+            this.rtbShow.Size = new System.Drawing.Size(1405, 578);
+            this.rtbShow.TabIndex = 1;
+            this.rtbShow.Text = "";
+            this.rtbShow.WordWrap = false;
+            // 
+            // btRead
+            // 
+            this.btRead.Location = new System.Drawing.Point(15, 13);
+            this.btRead.Name = "btRead";
+            this.btRead.Size = new System.Drawing.Size(103, 31);
+            this.btRead.TabIndex = 0;
+            this.btRead.Text = "read as text file";
+            this.btRead.UseVisualStyleBackColor = true;
+            this.btRead.Click += new System.EventHandler(this.btRead_Click);
+            // 
+            // tabpCSV
+            // 
+            this.tabpCSV.Controls.Add(this.dgvData);
+            this.tabpCSV.Controls.Add(this.btReadCSV);
+            this.tabpCSV.Location = new System.Drawing.Point(4, 25);
+            this.tabpCSV.Name = "tabpCSV";
+            this.tabpCSV.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabpCSV.Size = new System.Drawing.Size(1423, 860);
+            this.tabpCSV.TabIndex = 1;
+            this.tabpCSV.Text = "CSV";
+            this.tabpCSV.UseVisualStyleBackColor = true;
+            // 
+            // dgvData
+            // 
+            this.dgvData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.RegisterDate,
+            this.SHA256_Hash,
+            this.MD5_Hash,
+            this.SHA1_Hash});
+            this.dgvData.Location = new System.Drawing.Point(22, 63);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.RowHeadersWidth = 62;
+            this.dgvData.RowTemplate.Height = 28;
+            this.dgvData.Size = new System.Drawing.Size(1382, 785);
+            this.dgvData.TabIndex = 1;
+            // 
+            // RegisterDate
+            // 
+            this.RegisterDate.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.RegisterDate.HeaderText = "Date";
+            this.RegisterDate.MinimumWidth = 8;
+            this.RegisterDate.Name = "RegisterDate";
+            this.RegisterDate.Width = 65;
+            // 
+            // SHA256_Hash
+            // 
+            this.SHA256_Hash.HeaderText = "SHA256 Hash";
+            this.SHA256_Hash.MinimumWidth = 8;
+            this.SHA256_Hash.Name = "SHA256_Hash";
+            this.SHA256_Hash.Width = 150;
+            // 
+            // MD5_Hash
+            // 
+            this.MD5_Hash.HeaderText = "MD5Hash";
+            this.MD5_Hash.MinimumWidth = 8;
+            this.MD5_Hash.Name = "MD5_Hash";
+            this.MD5_Hash.Width = 150;
+            // 
+            // SHA1_Hash
+            // 
+            this.SHA1_Hash.HeaderText = "SHA1 Hash";
+            this.SHA1_Hash.MinimumWidth = 8;
+            this.SHA1_Hash.Name = "SHA1_Hash";
+            this.SHA1_Hash.Width = 150;
+            // 
+            // btReadCSV
+            // 
+            this.btReadCSV.Location = new System.Drawing.Point(20, 5);
+            this.btReadCSV.Name = "btReadCSV";
+            this.btReadCSV.Size = new System.Drawing.Size(95, 29);
+            this.btReadCSV.TabIndex = 0;
+            this.btReadCSV.Text = "read as csv";
+            this.btReadCSV.UseVisualStyleBackColor = true;
+            this.btReadCSV.Click += new System.EventHandler(this.btReadCSV_Click);
+            // 
+            // frmTextView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1452, 703);
+            this.Controls.Add(this.tabcMain);
+            this.Controls.Add(this.btBrowse);
+            this.Controls.Add(this.tbFileName);
+            this.Controls.Add(this.label1);
+            this.Name = "frmTextView";
+            this.Text = "Text/CSV viewer";
+            this.tabcMain.ResumeLayout(false);
+            this.tabpText.ResumeLayout(false);
+            this.tabpCSV.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox tbFileName;
+		private System.Windows.Forms.Button btBrowse;
+		private System.Windows.Forms.TabControl tabcMain;
+		private System.Windows.Forms.TabPage tabpText;
+		private System.Windows.Forms.TabPage tabpCSV;
+		private System.Windows.Forms.Button btRead;
+		private System.Windows.Forms.RichTextBox rtbShow;
+		private System.Windows.Forms.DataGridView dgvData;
+		private System.Windows.Forms.Button btReadCSV;
+		private System.Windows.Forms.DataGridViewTextBoxColumn RegisterDate;
+		private System.Windows.Forms.DataGridViewTextBoxColumn SHA256_Hash;
+		private System.Windows.Forms.DataGridViewTextBoxColumn MD5_Hash;
+		private System.Windows.Forms.DataGridViewTextBoxColumn SHA1_Hash;
+	}
+}
+
---
+++ TextCSV_Viewer.cs
@@ -0,0 +1,212 @@
+﻿/*
+MIT License
+
+Copyright (c) 2026 Sarayut Chaisuriya
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FileProcessing
+{
+    public partial class frmTextView : Form
+    {
+        private TextBox tbStartRow;
+        private TextBox tbEndRow;
+        private TextBox tbFilterType;
+        private Button btReadPartialCSV;
+
+        /// <summary>
+        /// Initializes a new instance of the frmTextView class.
+        /// </summary>
+        public frmTextView()
+        {
+            InitializeComponent();
+            SetupPartialControls();
+        }
+
+        private void SetupPartialControls()
+        {
+            // Add controls to CSV tab for HW4 requirements
+            Label lblStart = new Label { Text = "Start m:", Location = new System.Drawing.Point(170, 12), AutoSize = true };
+            tbStartRow = new TextBox { Location = new System.Drawing.Point(250, 8), Size = new System.Drawing.Size(80, 29), Text = "1" };
+
+            Label lblEnd = new Label { Text = "End n:", Location = new System.Drawing.Point(340, 12), AutoSize = true };
+            tbEndRow = new TextBox { Location = new System.Drawing.Point(420, 8), Size = new System.Drawing.Size(80, 29), Text = "100" };
+
+            Label lblFilter = new Label { Text = "Filter (e.g. exe):", Location = new System.Drawing.Point(510, 12), AutoSize = true };
+            tbFilterType = new TextBox { Location = new System.Drawing.Point(680, 8), Size = new System.Drawing.Size(130, 29), Text = "" };
+
+            btReadPartialCSV = new Button
+            {
+                Text = "Read Partial + Filter",
+                Location = new System.Drawing.Point(820, 5),
+                Size = new System.Drawing.Size(200, 40)
+            };
+            btReadPartialCSV.Click += new System.EventHandler(this.btReadPartialCSV_Click);
+
+            tabpCSV.Controls.Add(lblStart);
+            tabpCSV.Controls.Add(tbStartRow);
+            tabpCSV.Controls.Add(lblEnd);
+            tabpCSV.Controls.Add(tbEndRow);
+            tabpCSV.Controls.Add(lblFilter);
+            tabpCSV.Controls.Add(tbFilterType);
+            tabpCSV.Controls.Add(btReadPartialCSV);
+        }
+
+        private void btRead_Click(object sender, EventArgs e)
+        {
+            string content = File.ReadAllText(tbFileName.Text);
+            rtbShow.Text = content;
+        }
+
+        private void btReadCSV_Click(object sender, EventArgs e)
+        {
+            dgvData.Rows.Clear();
+            dgvData.Columns.Clear();
+
+            using (StreamReader srReader = new StreamReader(tbFileName.Text))
+            {
+                string strLine;
+                bool bHeaderRead = false;
+
+                while ((strLine = srReader.ReadLine()) != null)
+                {
+                    string[] strHeaders_arr = null;
+                    if (strLine.StartsWith("#"))
+                    {
+                        if (strLine.Length > 8 && strLine.Substring(0, 8).Equals("#HEADER"))
+                        {
+                            strHeaders_arr = strLine.Substring(8).Split(',');
+                        }
+                        continue;
+                    }
+
+                    string[] strValues_arr = strLine.Split(',');
+
+                    if (!bHeaderRead)
+                    {
+                        foreach (string strHeader in strValues_arr)
+                        {
+                            string colName = strHeader.Trim();
+                            if (strHeaders_arr != null && dgvData.Columns.Count < strHeaders_arr.Length)
+                                colName = strHeaders_arr[dgvData.Columns.Count].Trim();
+                            dgvData.Columns.Add(colName, colName);
+                        }
+                        bHeaderRead = true;
+                    }
+                    else
+                    {
+                        dgvData.Rows.Add(strValues_arr);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// HW4: Partial Loading (m-n) + Filter by file_type_guess + Combined
+        /// </summary>
+        private void btReadPartialCSV_Click(object sender, EventArgs e)
+        {
+            dgvData.Rows.Clear();
+            dgvData.Columns.Clear();
+
+            int startRow = 1;
+            int endRow = int.MaxValue;
+            string filter = tbFilterType.Text.Trim().ToLower();
+
+            int.TryParse(tbStartRow.Text, out startRow);
+            int.TryParse(tbEndRow.Text, out endRow);
+            if (startRow < 1) startRow = 1;
+            if (endRow < startRow) endRow = int.MaxValue;
+
+            int currentRow = 0;
+            int fileTypeIndex = -1;
+
+            using (StreamReader srReader = new StreamReader(tbFileName.Text))
+            {
+                string strLine;
+                bool bHeaderRead = false;
+
+                while ((strLine = srReader.ReadLine()) != null)
+                {
+                    if (strLine.StartsWith("#"))
+                    {
+                        if (strLine.Length > 8 && strLine.Substring(0, 8).Equals("#HEADER"))
+                        {
+                            // Header logic handled below
+                        }
+                        continue;
+                    }
+
+                    string[] strValues_arr = strLine.Split(',');
+
+                    if (!bHeaderRead)
+                    {
+                        // Add columns and detect file_type_guess
+                        for (int i = 0; i < strValues_arr.Length; i++)
+                        {
+                            string colName = strValues_arr[i].Trim().Replace("\"", "");
+                            dgvData.Columns.Add(colName, colName);
+                            if (colName.ToLower().Contains("file_type_guess"))
+                                fileTypeIndex = i;
+                        }
+                        bHeaderRead = true;
+                        continue;
+                    }
+
+                    currentRow++;
+
+                    if (currentRow < startRow) continue;
+                    if (currentRow > endRow) break;
+
+                    bool matches = true;
+                    if (!string.IsNullOrEmpty(filter) && fileTypeIndex >= 0)
+                    {
+                        string typeVal = strValues_arr[fileTypeIndex].Trim().ToLower().Replace("\"", "");
+                        if (!typeVal.Contains(filter))
+                            matches = false;
+                    }
+
+                    if (matches)
+                        dgvData.Rows.Add(strValues_arr);
+                }
+            }
+
+            MessageBox.Show($"Loaded {dgvData.Rows.Count} rows (range {startRow}-{endRow}, filter: '{filter}')", "Success");
+        }
+
+        private void btBrowse_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Text Files (*.txt)|*.txt|CSV Files (*.csv)|*.csv|All Files (*.*)|*.*";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    tbFileName.Text = ofd.FileName;
+                }
+            }
+        }
+
+        private void tbFileName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
